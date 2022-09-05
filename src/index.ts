68663b157b539696b08630f511c8b83e9aeba74a
bui bao long <blong1102@gmail.com>
2022-09-05 11:35:31 +0700

    setup resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import { ApolloServer } from 'apollo-server';
 
+import typeDefs from './schema';
+import { Query } from './resolvers';
+
+const resolvers = {
+  Query,
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is ready on ${url}`);
+});
